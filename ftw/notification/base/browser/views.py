1640e0ff9b6757b58f073ab8a18d0b11999c0a4e
Victor Baumann <v.baumann@4teamwork.ch>
2009-04-24 15:23:27 +0000

    redirect fix

--- ftw/notification/base/browser/views.py
+++ ftw/notification/base/browser/views.py
@@ -3,25 +3,23 @@ from Products.CMFCore.utils import getToolByName
 from zope.event import notify
 from ftw.notification.base.events.events import NotificationEvent
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.statusmessages.interfaces import IStatusMessage
+from ftw.notification.base import notification_base_factory as _
 
 
 
 class NotificationForm(BrowserView):
 
     template = ViewPageTemplateFile('notification_form.pt')
-
-    def __call__(self):
-
+        
+    def send_notification(self):
+        """"""
         sp = getToolByName(self.context, 'portal_properties').site_properties
-        use_view_action = self.context.Type() in  sp.getProperty('typesUseViewActionInListings', ())   
-
-        if self.request.get('submit'):
-            comment = self.request.get('comment', '')
-            notify(NotificationEvent(self.context, comment))
-            self.request.RESPONSE.redirect(self.context.absolute_url() + (use_view_action and '/view' or '') )
-        if self.request.get('cancel'):
-            self.request.RESPONSE.redirect(self.context.absolute_url() + (use_view_action and '/view' or '') )
-        return self.template()
+        use_view_action = self.context.Type() in  sp.getProperty('typesUseViewActionInListings', ())
+        comment = self.request.get('comment', '')
+        notify(NotificationEvent(self.context, comment))
+        IStatusMessage(self.request).addStatusMessage('statusmessage_notification_sent', type='info')
+        self.request.RESPONSE.redirect(self.context.absolute_url() + (use_view_action and '/view' or '') )
 
     def getAssignableUsers(self):
         """Collect users with a given role and return them in a list.
@@ -31,9 +29,10 @@ class NotificationForm(BrowserView):
         results = []
         pas_tool = getToolByName(context, 'acl_users')
         utils_tool = getToolByName(context, 'plone_utils')
+        
 
-        inherited_and_local_roles = utils_tool.getInheritedLocalRoles(context) + pas_tool.getLocalRolesForDisplay(context)
-
+        inherited_and_local_roles = utils_tool.getInheritedLocalRoles(self.context.aq_parent) + pas_tool.getLocalRolesForDisplay(self.context.aq_inner)
+            
         for user_id_and_roles in inherited_and_local_roles:
             if user_id_and_roles[2] == 'user':
                 if role in user_id_and_roles[1]:
