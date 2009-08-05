5f1353fd41592f46cb32fb8163ceb86664e0bb43
Julian Infanger <j.infanger@4teamwork.ch>
2009-08-05 14:57:31 +0000

    moved version from setup.py to version.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.2-dev'
+version = open('ftw/notification/base/version.txt').read().strip()
 maintainer = 'Victor Baumann'
 
 setup(name='ftw.notification.base',
