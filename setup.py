25d80d0abb22a5f4fd50ef2d14248a96f7c92577
Jonas Baumann <jone@jone.ch>
2011-12-01 09:27:42 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -2,35 +2,46 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.1dev'
-maintainer = 'Victor Baumann'
+maintainer = 'Jonas Baumann'
 
 setup(name='ftw.notification.base',
       version=version,
-      description="Maintainer: %s" % maintainer,
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description='Send notifications when editing a plone content.',
+      long_description=open('README.txt').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='',
-      author='%s, 4teamworkk GmbH' % maintainer,
+
+      keywords='ftw notification base plone',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.notification.base/',
+      url='https://github.com/4teamwork/ftw.notification.base',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', 'ftw.notification'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
+        'setuptools',
         'ftw.table',
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
+        ],
+
+      entry_points='''
       # -*- Entry points: -*-
-      """,
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
       )
