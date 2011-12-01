ae3aa124a3a254ece2662bb93a17a4073514bcfc
Jonas Baumann <jone@jone.ch>
2011-12-01 09:31:38 +0100

    Rename readme to .rst

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ maintainer = 'Jonas Baumann'
 setup(name='ftw.notification.base',
       version=version,
       description='Send notifications when editing a plone content.',
-      long_description=open('README.txt').read() + '\n' + \
+      long_description=open('README.rst').read() + '\n' + \
           open(os.path.join('docs', 'HISTORY.txt')).read(),
 
       # Get more strings from
