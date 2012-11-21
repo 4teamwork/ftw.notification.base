232d7eb5f89a6aac5da7e18ebcfff33fbd2a65ea
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-11-21 17:31:20 +0100

    email validation for the form

--- ftw/notification/base/browser/notification.py
+++ ftw/notification/base/browser/notification.py
@@ -1,9 +1,31 @@
-from Products.Five.browser import BrowserView
-import json
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.schema.vocabulary import getVocabularyRegistry
-from Products.CMFCore.utils import getToolByName
+from ftw.notification.base import notification_base_factory as _
 from ftw.notification.base.events.handlers import object_edited
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.statusmessages.interfaces import IStatusMessage
+from zope.schema.vocabulary import getVocabularyRegistry
+import json
+import re
+
+
+def extract_email(value):
+    if value:
+        return value.split(',')
+    return []
+
+
+def validmails(recipients):
+    valid = []
+    expr = re.compile(r"^(\w&.%#$&'\*+-/=?^_`{}|~]+!)*[\w&.%#$&'\*+-/=" +
+                      r"?^_`{}|~]+@(([0-9a-z]([0-9a-z-]*[0-9a-z])?" +
+                      r"\.)+[a-z]{2,6}|([0-9]{1,3}\.){3}[0-9]{1,3})$",
+                      re.IGNORECASE)
+
+    for mail in recipients:
+        if expr.match(mail):
+            valid.append(mail)
+    return valid
 
 
 class NotificationForm(BrowserView):
@@ -17,6 +39,26 @@ class NotificationForm(BrowserView):
         self.pre_select = []
 
     def __call__(self):
+        if self.request.get('form.submitted', False):
+            recipients = extract_email(self.request.get('users-to', ''))
+            import ipdb; ipdb.set_trace()
+            if len(recipients) == 0:
+                IStatusMessage(self.request).addStatusMessage(
+                    _(u'statusmessage_no_recipients',
+                      default=u"You have to select at least one recipient"),
+                    type='error')
+                return self.template()
+
+            if len(validmails(recipients)) != len(recipients):
+                IStatusMessage(self.request).addStatusMessage(
+                    _(u'statusmessage_invalid_mail',
+                      default=u"You entered one or more invalid recipient(s)"),
+                    type='error')
+                return self.template()
+
+            self.request.set('to_list', recipients)
+            self.send_notification()
+
         return self.template()
 
     def json_source(self):
@@ -88,5 +130,4 @@ class NotificationForm(BrowserView):
 
         # set sendNotification in REQUEST
         self.request.set('sendNotification', 1)
-        self.request.set('to_list', self.request.get('users-to'))
         object_edited(self.context, None)
